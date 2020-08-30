39ab735f7d7be3b62f97ea5aeff8c42653f5c50b
hadeel-k <41871869+hadeel-k@users.noreply.github.com>
2020-08-30 09:28:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -261,4 +261,4 @@ APP = create_app()
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))
-    APP.run(host='0.0.0.0', port=5432, debug=True)
+    APP.run(host='0.0.0.0', port=port, debug=True)
